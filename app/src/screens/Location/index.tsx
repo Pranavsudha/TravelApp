50a528b6d9621a7c29a588fdee23e98120f9e95b
Pranavan Sudhahar <pranavan@codelantic.com>
2023-12-29 22:25:09 +0530

    feat(Maps):Location Bottom Tab Screen Done

--- app/src/screens/Location/index.tsx
+++ app/src/screens/Location/index.tsx
@@ -1,14 +1,28 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import MapView, { Marker } from 'react-native-maps'
 
 const Location = () => {
+  const coordinates = {
+    latitude:35.6855,
+    longitude:139.68884,
+    latitudeDelta:0.01,
+    longitudeDelta:0.01,
+    title:'My Location'
+
+  }
   return (
-    <View>
-      <Text>Location</Text>
-    </View>
+   <MapView initialRegion={coordinates} style={styles.map}>
+    <Marker coordinate={coordinates} title={coordinates.title}/>
+   </MapView>
   )
 }
 
 export default Location
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  map:{
+ width:'100%',
+ height:"100%"
+  }
+})
